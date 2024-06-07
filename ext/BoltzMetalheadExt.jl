a9f7b3f78463b9b4e4d5a611902f515bb7013791
Avik Pal <avikpal@mit.edu>
2024-06-06 19:17:45 -0700

    Stop using deprecated API

--- ext/BoltzMetalheadExt.jl
+++ ext/BoltzMetalheadExt.jl
@@ -6,7 +6,7 @@ using Boltz: _initialize_model, assert_name_present_in
 
 function alexnet(name::Symbol; pretrained=false, kwargs...)
     assert_name_present_in(name, (:alexnet,))
-    model = transform(AlexNet().layers)
+    model = FromFluxAdaptor()(AlexNet().layers)
 
     # Compatibility with pretrained weights
     pretrained && (model = Chain(model[1], model[2]))
@@ -17,15 +17,15 @@ end
 function resnet(name::Symbol; pretrained=false, kwargs...)
     assert_name_present_in(name, (:resnet18, :resnet34, :resnet50, :resnet101, :resnet152))
     model = if name == :resnet18
-        transform(ResNet(18).layers)
+        FromFluxAdaptor()(ResNet(18).layers)
     elseif name == :resnet34
-        transform(ResNet(34).layers)
+        FromFluxAdaptor()(ResNet(34).layers)
     elseif name == :resnet50
-        transform(ResNet(50).layers)
+        FromFluxAdaptor()(ResNet(50).layers)
     elseif name == :resnet101
-        transform(ResNet(101).layers)
+        FromFluxAdaptor()(ResNet(101).layers)
     elseif name == :resnet152
-        transform(ResNet(152).layers)
+        FromFluxAdaptor()(ResNet(152).layers)
     end
 
     return _initialize_model(name, model; pretrained, kwargs...)
@@ -34,31 +34,31 @@ end
 function resnext(name::Symbol; kwargs...)
     assert_name_present_in(name, (:resnext50, :resnext101, :resnext152))
     model = if name == :resnext50
-        transform(ResNeXt(50).layers)
+        FromFluxAdaptor()(ResNeXt(50).layers)
     elseif name == :resnext101
-        transform(ResNeXt(101).layers)
+        FromFluxAdaptor()(ResNeXt(101).layers)
     elseif name == :resnext152
-        transform(ResNeXt(152).layers)
+        FromFluxAdaptor()(ResNeXt(152).layers)
     end
     return _initialize_model(name, model; kwargs...)
 end
 
 function googlenet(name::Symbol; kwargs...)
     assert_name_present_in(name, (:googlenet,))
-    model = transform(GoogLeNet().layers)
+    model = FromFluxAdaptor()(GoogLeNet().layers)
     return _initialize_model(name, model; kwargs...)
 end
 
 function densenet(name::Symbol; kwargs...)
     assert_name_present_in(name, (:densenet121, :densenet161, :densenet169, :densenet201))
     model = if name == :densenet121
-        transform(DenseNet(121).layers)
+        FromFluxAdaptor()(DenseNet(121).layers)
     elseif name == :densenet161
-        transform(DenseNet(161).layers)
+        FromFluxAdaptor()(DenseNet(161).layers)
     elseif name == :densenet169
-        transform(DenseNet(169).layers)
+        FromFluxAdaptor()(DenseNet(169).layers)
     elseif name == :densenet201
-        transform(DenseNet(201).layers)
+        FromFluxAdaptor()(DenseNet(201).layers)
     end
     return _initialize_model(name, model; kwargs...)
 end
@@ -67,20 +67,20 @@ function mobilenet(name::Symbol; kwargs...)
     assert_name_present_in(name,
         (:mobilenet_v1, :mobilenet_v2, :mobilenet_v3_small, :mobilenet_v3_large))
     model = if name == :mobilenet_v1
-        transform(MobileNetv1().layers)
+        FromFluxAdaptor()(MobileNetv1().layers)
     elseif name == :mobilenet_v2
-        transform(MobileNetv2().layers)
+        FromFluxAdaptor()(MobileNetv2().layers)
     elseif name == :mobilenet_v3_small
-        transform(MobileNetv3(:small).layers)
+        FromFluxAdaptor()(MobileNetv3(:small).layers)
     elseif name == :mobilenet_v3_large
-        transform(MobileNetv3(:large).layers)
+        FromFluxAdaptor()(MobileNetv3(:large).layers)
     end
     return _initialize_model(name, model; kwargs...)
 end
 
 function convmixer(name::Symbol; kwargs...)
     assert_name_present_in(name, (:base, :large, :small))
-    model = transform(ConvMixer(name).layers)
+    model = FromFluxAdaptor()(ConvMixer(name).layers)
     return _initialize_model(name, model; kwargs...)
 end
 
