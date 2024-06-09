7c074f9ea16d570f0f936cbdbf71b3c9ef60b8e2
Avik Pal <avikpal@mit.edu>
2024-06-09 16:36:06 -0700

    Add Chebyshev KANs

---
+++ src/kan/KAN.jl
@@ -0,0 +1,17 @@
+module KAN
+
+using PrecompileTools: @recompile_invalidations
+
+@recompile_invalidations begin
+    using ArgCheck: @argcheck
+    using ..Boltz: Boltz, Basis
+    using ConcreteStructs: @concrete
+    using LuxCore: AbstractExplicitLayer, LuxCore
+    using NNlib: NNlib
+    using Random: Random, AbstractRNG
+    using WeightInitializers: randn32
+end
+
+include("dense.jl")
+
+end
