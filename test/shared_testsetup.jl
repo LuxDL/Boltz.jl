c1e89864d5c4de8ce2f5c7c6ea8a160c0b5e2c6d
Avik Pal <avikpal@mit.edu>
2024-07-16 22:48:02 -0700

    test: lazy install cuda and amdgpu

--- test/shared_testsetup.jl
+++ test/shared_testsetup.jl
@@ -1,30 +1,38 @@
 @testsetup module SharedTestSetup
 
 import Reexport: @reexport
-@reexport using Boltz, Lux, LuxCUDA, LuxAMDGPU, LuxLib, LuxTestUtils, Random
+@reexport using Boltz, Lux, GPUArraysCore, LuxLib, LuxTestUtils, Random
 import Metalhead
 
-const BACKEND_GROUP = get(ENV, "BACKEND_GROUP", "All")
+LuxTestUtils.jet_target_modules!(["Boltz", "Lux", "LuxLib"])
 
-CUDA.allowscalar(false)
+const BACKEND_GROUP = lowercase(get(ENV, "BACKEND_GROUP", "all"))
 
-cpu_testing() = BACKEND_GROUP == "All" || BACKEND_GROUP == "CPU"
-cuda_testing() = (BACKEND_GROUP == "All" || BACKEND_GROUP == "CUDA") && LuxCUDA.functional()
+GPUArraysCore.allowscalar(false)
+
+if BACKEND_GROUP == "all" || BACKEND_GROUP == "cuda"
+    using LuxCUDA
+end
+
+if BACKEND_GROUP == "all" || BACKEND_GROUP == "amdgpu"
+    using AMDGPU
+end
+
+cpu_testing() = BACKEND_GROUP == "all" || BACKEND_GROUP == "cpu"
+function cuda_testing()
+    return (BACKEND_GROUP == "all" || BACKEND_GROUP == "cuda") &&
+           LuxDeviceUtils.functional(LuxCUDADevice)
+end
 function amdgpu_testing()
-    (BACKEND_GROUP == "All" || BACKEND_GROUP == "AMDGPU") && LuxAMDGPU.functional()
+    return (BACKEND_GROUP == "all" || BACKEND_GROUP == "amdgpu") &&
+           LuxDeviceUtils.functional(LuxAMDGPUDevice)
 end
 
 const MODES = begin
-    # Mode, Array Type, Device Function, GPU?
-    cpu_mode = ("CPU", Array, LuxCPUDevice(), false)
-    cuda_mode = ("CUDA", CuArray, LuxCUDADevice(), true)
-    amdgpu_mode = ("AMDGPU", ROCArray, LuxAMDGPUDevice(), true)
-
     modes = []
-    cpu_testing() && push!(modes, cpu_mode)
-    cuda_testing() && push!(modes, cuda_mode)
-    amdgpu_testing() && push!(modes, amdgpu_mode)
-
+    cpu_testing() && push!(modes, ("cpu", Array, LuxCPUDevice(), false))
+    cuda_testing() && push!(modes, ("cuda", CuArray, LuxCUDADevice(), true))
+    amdgpu_testing() && push!(modes, ("amdgpu", ROCArray, LuxAMDGPUDevice(), true))
     modes
 end
 
