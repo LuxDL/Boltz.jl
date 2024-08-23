6d4973ae317381bee1f6b4fa4c0f22ff52684418
Avik Pal <avikpal@mit.edu>
2024-08-23 16:31:31 -0700

    refactor: cleanup data interpolations

--- ext/BoltzDataInterpolationsExt.jl
+++ ext/BoltzDataInterpolationsExt.jl
@@ -1,28 +1,29 @@
 module BoltzDataInterpolationsExt
 
-using Boltz: Boltz, Layers, Utils
 using DataInterpolations: AbstractInterpolation
+
+using Boltz: Boltz, Layers, Utils
 
 for train_grid in (true, false)
     grid_expr = train_grid ? :(grid = ps.grid) : :(grid = st.grid)
     @eval function (spl::Layers.SplineLayer{$(train_grid), Basis})(
             t::AbstractVector, ps, st) where {Basis <: AbstractInterpolation}
         $(grid_expr)
-        interp = __construct_basis(Basis, ps.saved_points, grid; extrapolate=true)
+        interp = construct_basis(Basis, ps.saved_points, grid; extrapolate=true)
         sol = interp.(t)
         spl.in_dims == () && return sol, st
         return Utils.mapreduce_stack(sol), st
     end
 end
 
-@inline function __construct_basis(
+function construct_basis(
         ::Type{Basis}, saved_points::AbstractVector, grid; extrapolate=false) where {Basis}
     return Basis(saved_points, grid; extrapolate)
 end
 
-@inline function __construct_basis(::Type{Basis}, saved_points::AbstractArray{T, N},
+function construct_basis(::Type{Basis}, saved_points::AbstractArray{T, N},
         grid; extrapolate=false) where {Basis, T, N}
-    return __construct_basis(
+    return construct_basis(
         # Unfortunately DataInterpolations.jl is not very robust to different array types
         # so we have to make a copy
         Basis, [copy(selectdim(saved_points, N, i)) for i in 1:size(saved_points, N)],
