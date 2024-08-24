56005318c0dd47383163a75252490da8e4b5b03f
Avik Pal <avikpal@mit.edu>
2024-08-24 00:53:06 -0700

    ci: parallel CPU testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,11 +19,15 @@ end
 using Boltz
 
 const BOLTZ_TEST_GROUP = get(ENV, "BOLTZ_TEST_GROUP", "all")
+const RETESTITEMS_NWORKERS = parse(
+    Int, get(ENV, "RETESTITEMS_NWORKERS", string(min(Hwloc.num_physical_cores(), 16))))
 const RETESTITEMS_NWORKER_THREADS = parse(Int,
-    get(ENV, "RETESTITEMS_NWORKER_THREADS", string(max(Hwloc.num_virtual_cores(), 1))))
+    get(ENV, "RETESTITEMS_NWORKER_THREADS",
+        string(max(Hwloc.num_virtual_cores() ÷ RETESTITEMS_NWORKERS, 1))))
 
-@info "Running tests for group: $BOLTZ_TEST_GROUP"
+@info "Running tests for group: $BOLTZ_TEST_GROUP with $RETESTITEMS_NWORKERS workers"
 
 ReTestItems.runtests(
     Boltz; tags=(BOLTZ_TEST_GROUP == "all" ? nothing : [Symbol(BOLTZ_TEST_GROUP)]),
-    nworkers=0, nworker_threads=RETESTITEMS_NWORKER_THREADS, testitem_timeout=3600)
+    nworkers=ifelse(BACKEND_GROUP ∈ ("cuda", "amdgpu"), 0, RETESTITEMS_NWORKERS),
+    nworker_threads=RETESTITEMS_NWORKER_THREADS, testitem_timeout=3600)
