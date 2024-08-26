1379cb3bd6c689a20643dbd4897e719b4b5bee25
Avik Pal <avikpal@mit.edu>
2024-08-26 17:41:18 -0400

    feat!: remove a direct dependency on Boltz.jl

--- src/Boltz.jl
+++ src/Boltz.jl
@@ -1,8 +1,6 @@
 module Boltz
 
 using ArgCheck: @argcheck
-using JLD2: JLD2 # TODO[BREAKING]: Remove JLD2 dependency and ask users to load it
-using Random: Random
 using Reexport: @reexport
 
 @reexport using Lux
@@ -10,7 +8,6 @@ using Reexport: @reexport
 # Utility Functions
 include("utils.jl")
 include("initialize.jl")
-include("patch.jl")
 
 # Basis Functions
 include("basis.jl")
@@ -20,9 +17,6 @@ include("layers/Layers.jl")
 
 # Vision Models
 include("vision/Vision.jl")
-
-# deprecated
-include("deprecated.jl")
 
 export Basis, Layers, Vision
 
