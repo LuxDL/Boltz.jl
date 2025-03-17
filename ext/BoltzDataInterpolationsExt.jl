5de74e0e9fb94d2486c4761e9d0f809910a4c82e
Avik Pal <avikpal@mit.edu>
2025-03-17 00:28:27 -0400

    Revert "feat: support new DataInterpolations (#109)"
    This reverts commit ee3541bc5f2416d9faea6b1065143c1d2ceb072c.

--- ext/BoltzDataInterpolationsExt.jl
+++ ext/BoltzDataInterpolationsExt.jl
@@ -1,6 +1,6 @@
 module BoltzDataInterpolationsExt
 
-using DataInterpolations: AbstractInterpolation, ExtrapolationType
+using DataInterpolations: AbstractInterpolation
 
 using Boltz: Boltz, Layers, Utils
 
@@ -11,9 +11,7 @@ for train_grid in (true, false), tType in (AbstractVector, Number)
         t::$(tType), ps, st
     ) where {Basis<:AbstractInterpolation}
         $(grid_expr)
-        interp = construct_basis(
-            Basis, ps.saved_points, grid; extrapolation=ExtrapolationType.Constant
-        )
+        interp = construct_basis(Basis, ps.saved_points, grid; extrapolate=true)
         $(sol_expr)
         spl.in_dims == () && return sol, st
         return Utils.mapreduce_stack(sol), st
@@ -21,13 +19,13 @@ for train_grid in (true, false), tType in (AbstractVector, Number)
 end
 
 function construct_basis(
-    ::Type{Basis}, saved_points::AbstractVector, grid; kwargs...
+    ::Type{Basis}, saved_points::AbstractVector, grid; extrapolate=false
 ) where {Basis}
-    return Basis(saved_points, grid; kwargs...)
+    return Basis(saved_points, grid; extrapolate)
 end
 
 function construct_basis(
-    ::Type{Basis}, saved_points::AbstractArray{T,N}, grid; kwargs...
+    ::Type{Basis}, saved_points::AbstractArray{T,N}, grid; extrapolate=false
 ) where {Basis,T,N}
     return construct_basis(
         # Unfortunately DataInterpolations.jl is not very robust to different array types
@@ -35,7 +33,7 @@ function construct_basis(
         Basis,
         [copy(selectdim(saved_points, N, i)) for i in 1:size(saved_points, N)],
         grid;
-        kwargs...,
+        extrapolate,
     )
 end
 
