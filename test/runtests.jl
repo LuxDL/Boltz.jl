7c13cf7804d42037eebe4e49c3fb448ac1e9f68a
Avik Pal <avikpal@mit.edu>
2025-08-02 17:12:21 -0400

    ci: update common ci (#140)
    * ci: update common ci
    
    * fix: temporary workaround for mapreduce

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,26 @@ const ALL_BOTLZ_TEST_GROUPS = [
     "layers", "others", "vision", "vision_metalhead", "integration", "piml"
 ]
 
-const BOLTZ_TEST_REACTANT = parse(Bool, lowercase(get(ENV, "BOLTZ_TEST_REACTANT", "true")))
+function parse_test_args()
+    test_args_from_env = @isdefined(TEST_ARGS) ? TEST_ARGS : ARGS
+    test_args = Dict{String,String}()
+    for arg in test_args_from_env
+        if contains(arg, "=")
+            key, value = split(arg, "="; limit=2)
+            test_args[key] = value
+        end
+    end
+    @info "Parsed test args" test_args
+    return test_args
+end
 
-INPUT_TEST_GROUP = lowercase(get(ENV, "BOLTZ_TEST_GROUP", "all"))
+const PARSED_TEST_ARGS = parse_test_args()
+
+const BOLTZ_TEST_REACTANT = parse(
+    Bool, lowercase(get(PARSED_TEST_ARGS, "BOLTZ_TEST_REACTANT", "true"))
+)
+
+INPUT_TEST_GROUP = lowercase(get(PARSED_TEST_ARGS, "BOLTZ_TEST_GROUP", "all"))
 const BOLTZ_TEST_GROUP = if startswith("!", INPUT_TEST_GROUP[1])
     exclude_group = lowercase.(split(INPUT_TEST_GROUP[2:end], ","))
     filter(x -> x ∉ exclude_group, ALL_BOTLZ_TEST_GROUPS)
@@ -14,7 +31,7 @@ else
     [INPUT_TEST_GROUP]
 end
 
-const BACKEND_GROUP = lowercase(get(ENV, "BACKEND_GROUP", "all"))
+const BACKEND_GROUP = lowercase(get(PARSED_TEST_ARGS, "BACKEND_GROUP", "all"))
 const EXTRA_PKGS = String[]
 
 if "all" ∈ BOLTZ_TEST_GROUP || "integration" ∈ BOLTZ_TEST_GROUP
@@ -46,15 +63,23 @@ using Boltz
 
 @testset "Boltz.jl Tests" begin
     @testset for (i, tag) in enumerate(BOLTZ_TEST_GROUP)
-        ReTestItems.runtests(
-            Boltz;
-            tags=(tag == "all" ? nothing : [Symbol(tag)]),
-            testitem_timeout=2400,
-            nworkers=0,
-            nworker_threads=parse(
-                Int,
-                get(ENV, "RETESTITEMS_NWORKER_THREADS", string(Hwloc.num_virtual_cores())),
-            ),
-        )
+        withenv(
+            "BOLTZ_TEST_REACTANT" => BOLTZ_TEST_REACTANT, "BACKEND_GROUP" => BACKEND_GROUP
+        ) do
+            ReTestItems.runtests(
+                Boltz;
+                tags=(tag == "all" ? nothing : [Symbol(tag)]),
+                testitem_timeout=2400,
+                nworkers=0,
+                nworker_threads=parse(
+                    Int,
+                    get(
+                        ENV,
+                        "RETESTITEMS_NWORKER_THREADS",
+                        string(Hwloc.num_virtual_cores()),
+                    ),
+                ),
+            )
+        end
     end
 end
