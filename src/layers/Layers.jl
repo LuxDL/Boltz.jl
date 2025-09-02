5e8544c102b0b9527fe910f5c2ea152fd60c7d31
Avik Pal <avikpal@mit.edu>
2025-09-02 15:16:10 -0400

    feat: improve codegen for physics attention

--- src/layers/Layers.jl
+++ src/layers/Layers.jl
@@ -13,6 +13,7 @@ using ForwardDiff: ForwardDiff
 
 using Lux: Lux, LuxOps, StatefulLuxLayer
 using LuxCore: LuxCore, AbstractLuxLayer, AbstractLuxContainerLayer, AbstractLuxWrapperLayer
+using LuxLib: batched_matmul
 using MLDataDevices: get_device, CPUDevice
 using NNlib: NNlib
 using WeightInitializers: zeros32, randn32, ones32, orthogonal
