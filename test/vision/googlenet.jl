67714260eaa7f1dcf402dec8e8cde583ceaab685
Avik Pal <avikpal@mit.edu>
2026-01-30 15:24:35 -0600

    test: missing loads

--- test/vision/googlenet.jl
+++ test/vision/googlenet.jl
@@ -1,8 +1,8 @@
-using Boltz, Test
+using Boltz, Test, Metalhead
 
 include("testutils.jl")
 
 @testset "GoogleNet: pretrained: $(pretrained)" for pretrained in [false]
-    model = Vision.GoogleNet(; pretrained)
+    model = Vision.GoogLeNet(; pretrained)
     VisionTestUtils.test_model(model; pretrained)
 end
