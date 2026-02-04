6c2ead3f923dabd0f649a5216e3e5069c34955ab
Avik Pal <avikpal@mit.edu>
2026-02-04 09:52:08 -0600

    test: up

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,24 @@
-using Boltz, ParallelTestRunner
+using Boltz, ParallelTestRunner, Setfield
+
+parsed_args = parse_args(@isdefined(TEST_ARGS) ? TEST_ARGS : ARGS)
 
 testsuite = find_tests(@__DIR__)
 delete!(testsuite, "testutils")
 delete!(testsuite, "vision/testutils")
 
 # Limit total jobs to 4 to avoid OOM on GPU
-total_jobs = min(ParallelTestRunner.default_njobs(), length(keys(testsuite)), 4)
+total_jobs = min(
+    something(parsed_args.jobs, ParallelTestRunner.default_njobs()),
+    length(keys(testsuite)),
+    4,
+)
 
-testrunner_args = @isdefined(TEST_ARGS) ? TEST_ARGS : ARGS
-push!(testrunner_args, "--jobs=$(total_jobs)")
+@set! parsed_args.jobs = Some(total_jobs)
 
 withenv(
     "XLA_REACTANT_GPU_MEM_FRACTION" => 1 / (total_jobs + 0.1),
     "XLA_REACTANT_GPU_PREALLOCATE" => false,
+    "JULIA_CUDA_HARD_MEMORY_LIMIT" => "$(100 / (total_jobs + 0.1))%",
 ) do
-    runtests(Boltz, testrunner_args; testsuite)
+    runtests(Boltz, parsed_args; testsuite)
 end
