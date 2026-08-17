5a93c60dbf2610474c38ac31c5f31fc98f64ab01
avik-pal <avikpal@mit.edu>
2026-08-17 04:10:14 +0000

    delete history

---
+++ versions.js
@@ -0,0 +1,13 @@
+var DOC_VERSIONS = [
+  "dev",
+  "stable",
+  "v1",
+  "v1.6",
+  "v1.6.1",
+  "v1.3.0",
+  "v1.0.3",
+  "v0.3.13",
+  "v0.3.12",
+];
+var DOCUMENTER_NEWEST = "1.7.1";
+var DOCUMENTER_STABLE = "stable";
